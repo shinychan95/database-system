514c5d2deedc396fbb777e4620e62e041ab62ec6
shinychan95 <shinychan95@gmail.com>
2021-03-14 19:19:20 +0900

    Add the AllocTrain code

--- project2/EduBfM_64bit/edubfm_AllocTrain.c
+++ project2/EduBfM_64bit/edubfm_AllocTrain.c
@@ -67,11 +67,19 @@ extern CfgParams_T sm_cfgParams;
  *  2) Error codes: Negative value means error code.
  *     eNOUNFIXEDBUF_BFM - There is no unfixed buffer.
  *     some errors caused by fuction calls
+ * 
+ * 설명:
+ *  bufferPool에서 page/train을 저장하기 위한 buffer element를 한 개 할당 받고, 
+ *  해당 buffer element의 array index를 반환함
+ * 
+ * 관련 함수:
+ *  1. edubfm_Delete() - hashTable에서 buffer element의 array index를 삭제함
+ *  2. edubfm_FlushTrain() - 수정된 page/train을 disk에 기록함
  */
 Four edubfm_AllocTrain(
     Four 	type)			/* IN type of buffer (PAGE or TRAIN) */
 {
-    Four 	e;			/* for error */
+    Four 	e;			    /* for error */
     Four 	victim;			/* return value */
     Four 	i;
     
@@ -79,8 +87,41 @@ Four edubfm_AllocTrain(
 	/* Error check whether using not supported functionality by EduBfM */
 	if(sm_cfgParams.useBulkFlush) ERR(eNOTSUPPORTED_EDUBFM);
 
+    victim = BI_NEXTVICTIM(type);
 
-    
+    // Second chance buffer replacement algorithm을 사용하여, 할당 받을 buffer element를 선정함
+    // 할당 대상 선정을 위해 대응하는 fixed 변수 값이 0인 buffer element들을 순차적으로 방문함
+    for (i = 0; i < BI_BUFSIZE(type) * 2; i++) {
+        if (BI_FIXED(type, victim) == 0) {
+            if (BI_BITS(type, victim) & REFER) {
+                BI_BITS(type, victim) ^= REFER;
+            }
+            else {
+                break;
+            }
+        }
+
+        victim = (victim + 1) % BI_BUFSIZE(type);
+    }
+
+    // 선정된 victim이 없을 경우 에러를 반환
+    if (i == BI_BUFSIZE(type) * 2) ERR(eNOUNFIXEDBUF_BFM);
+
+    // 선정된 buffer element와 관련된 데이터 구조를 초기화함
+    // 선정된 buffer element에 저장되어 있던 page/train이 수정된 경우, 기존 buffer element의 내용을 disk로 flush함
+    if (BI_BITS(type, victim) & DIRTY == DIRTY) {
+        e = edubfm_FlushTrain(&BI_KEY(type, victim), type);
+        if (e < 0) ERR(e);
+    }
+
+    // 선정된 buffer element에 대응하는 bufTable element를 초기화함
+    // 질문? - EduBfM_GetTrain에서 초기화하지 않나?
+
+    // 선정된 buffer element의 array index (hashTable entry) 를 hashTable에서 삭제함
+    e = edubfm_Delete(&BI_KEY(type, victim), type);
+    if (e < 0) ERR(e);
+
+    // 선정된 buffer element의 array index를 반환함
     return( victim );
     
 }  /* edubfm_AllocTrain */
