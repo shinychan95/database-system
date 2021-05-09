24dbe869f52336c6c661f344cc0ca9ba6d041d13
shinychan95 <shinychan95@gmail.com>
2021-05-09 18:14:40 +0900

    Finish the CreateIndex function

--- project4/EduBtM_64bit/EduBtM_CreateIndex.c
+++ project4/EduBtM_64bit/EduBtM_CreateIndex.c
@@ -58,7 +58,16 @@
  *    some errors caused by function calls
  *
  * Side effects:
- *  The parameter rootPid is filled with the new root page's PageID. 
+ *  The parameter rootPid is filled with the new root page's PageID.
+ * 
+ * 한글 설명:
+ *  색인 file에서 새로운 B+ tree 색인을 생성하고, 생성된 색인의 root page의 page ID를 반환함
+ * 
+ * 관련 함수:
+ *   - edubtm_InitLeaf(): Page를 B+ tree 색인의 leaf page로 초기화함
+ *   - btm_AllocPage(): B+ tree 색인 page로 사용할 새로운 page를 할당 하고, 할당된 page의 page ID를 반환함
+ *   - BfM_GetTrain()
+ *   - BfM_FreeTrain()
  */
 Four EduBtM_CreateIndex(
     ObjectID *catObjForFile,	/* IN catalog object of B+ tree file */
@@ -70,7 +79,25 @@ Four EduBtM_CreateIndex(
     sm_CatOverlayForBtree *catEntry; /* pointer to Btree file catalog information */
     PhysicalFileID pFid;	/* physical file ID */
 
+    // catObjForFile(sm_CatOverlayForSysTables)에 대한 페이지를 buffer에 fix한다.
+    e = BfM_GetTrain((TrainID*)catObjForFile, (char**)&catPage, PAGE_BUF);
+    if (e < eNOERROR) ERR(e);
 
+    // catEntry가 sm_CatOverlayForSysTables 내 btree를 가리키도록 설정한다. 
+    GET_PTR_TO_CATENTRY_FOR_BTREE(catObjForFile, catPage, catEntry);
+    
+    MAKE_PHYSICALFILEID(pFid, catEntry->fid.volNo, catEntry->firstPage);
+
+    // btm_AllocPage()를 호출하여 색인 file의 첫 번째 page를 할당 받음
+	e = btm_AllocPage(catObjForFile, (PageID *)&pFid, rootPid);
+	if (e < eNOERROR) ERR(e);
+
+    // 할당 받은 page를 root page로 초기화함 (root = TRUE, isTmp = FALSE)
+    e = edubtm_InitLeaf(rootPid, TRUE, FALSE);
+    if (e < eNOERROR) ERR(e);
+
+    e = BfM_FreeTrain((TrainID*)catObjForFile, PAGE_BUF);
+	if (e < eNOERROR) ERR(e);
 
     return(eNOERROR);
     
